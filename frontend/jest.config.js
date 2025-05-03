83065b2e2c95db96337f2c8ccbeb7e3d9ff7fae5
Otto Jung <otto.jung@vauplace.com>
2025-05-02 17:19:33 -0700

    fix linting problems

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   displayName: 'frontend',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\.[jt]sx?$': 'babel-jest'
+    '^.+[.][jt]sx?$': 'babel-jest'
   },
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
