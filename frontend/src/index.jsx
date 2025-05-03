d4b67618e8b37d092247122d06e20983053ddfbc
Otto Jung <otto.jung@vauplace.com>
2025-05-02 17:23:13 -0700

    integrate chakraui

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Camera from './Camera';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/camera" element={<Camera />} />
-        <Route path="/camera.html" element={<Camera />} />
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/camera" element={<Camera />} />
+          <Route path="/camera.html" element={<Camera />} />
+          <Route path="*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
