0b83e381318a9fdf808f9b818d95a2c1fb8e9702
Otto Jung <otto.jung@vauplace.com>
2025-05-03 21:44:58 -0700

    use correct port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const port = parseInt(process.env.VOLODYSLAV_SERVER_PORT);
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
        // Proxy upload API calls to backend.
       '/api/upload': {
-        target: 'http://localhost:29932',
+        target: `http://localhost:${port}`,
         changeOrigin: true,
       },
     },
