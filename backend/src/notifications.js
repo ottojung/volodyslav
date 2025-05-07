a48f6fa726d7739d745391bccd80c91dc0d12299
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-07 18:09:54 +0000

    initial notification system

--- backend/src/notifications.js
+++ backend/src/notifications.js
@@ -1,11 +1,45 @@
-const { execFile } = require('child_process');
+const { execFile } = require("child_process");
+
+/**
+ * This function resolves the path to the termux-notification executable.
+ *
+ * @returns {string|null} - The path to the termux-notification executable or null if not found.
+ */
+function resolveTermuxNotificationPath() {
+    // Return path to termux-notification executable.
+    execFile("command", ["-v", "termux-notification"], (error, stdout) => {
+        if (error) {
+            console.error("termux-notification not found in PATH");
+            return null;
+        }
+        return stdout.trim();
+    });
+}
+
+class TermuxNotificationError extends Error {
+    constructor() {
+        super(
+            "Termux notification executable not found in PATH. Please ensure that termux-notification is installed and available in your PATH."
+        );
+    }
+}
+
+/**
+ * Ensures that the termux-notification executable exists in the PATH.
+ */
+function ensureTermuxNotificationExists() {
+    const termuxNotificationPath = resolveTermuxNotificationPath();
+    if (!termuxNotificationPath) {
+        throw new TermuxNotificationError();
+    }
+}
 
 /**
  * Sends an error notification using termux-notification.
  * @param {string} message - The error message to display.
  */
 function notifyAboutError(message) {
-    execFile('termux-notification', ['-t', 'Error', '-c', message]);
+    execFile("termux-notification", ["-t", "Error", "-c", message]);
 }
 
 /**
@@ -13,10 +47,11 @@ function notifyAboutError(message) {
  * @param {string} message - The warning message to display.
  */
 function notifyAboutWarning(message) {
-    execFile('termux-notification', ['-t', 'Warning', '-c', message]);
+    execFile("termux-notification", ["-t", "Warning", "-c", message]);
 }
 
 module.exports = {
+    ensureTermuxNotificationExists,
     notifyAboutError,
-    notifyAboutWarning
+    notifyAboutWarning,
 };
