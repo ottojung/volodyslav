5f126972487985a2337358fb7dd45adc8ca60983
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-08 23:56:29 +0000

    refactor: restructure transaction function and improve temporary work tree handling

--- backend/src/gitstore/index.js
+++ backend/src/gitstore/index.js
@@ -1,5 +1,36 @@
+const os = require("os");
+const { reset, commit } = require("./wrappers");
+const fs = require("fs").promises;
 
-async function transaction(transformation) {
+async function makeTemporaryWorkTree() {
+    return await fs.mkdtemp(`${os.tmpdir()}/gitstore-`);
+}
+
+/**
+ * This function performs a transaction on a Git repository.
+ * 
+ * It gives you a temporary work tree, reset to the last commit,
+ * and allows you to perform a transformation on it.
+ * 
+ * It is atomic: if the transformation fails, the changes are not committed.
+ * 
+ * @param {string} git_directory - The `.git` directory
+ * @param {function(string): Promise<void>} transformation - A function that takes a directory path and performs some operations on it
+ * @returns {Promise<void>}
+ */
+async function transaction(git_directory, transformation) {
+    const temporaryWorkTree = await makeTemporaryWorkTree();
+    try {
+        reset(git_directory, temporaryWorkTree);
+        await transformation(temporaryWorkTree);
+        await commit(
+            git_directory,
+            temporaryWorkTree,
+            "Diary audio files transcribed"
+        );
+    } finally {
+        await fs.rm(temporaryWorkTree, { recursive: true, force: true });
+    }
 }
 
 module.exports = {
