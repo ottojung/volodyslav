bb55790f8d28f7fb3c2953eb41ff39c801efc519
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-08 21:44:41 +0000

    fix: add git command registration to executables module

--- backend/src/executables.js
+++ backend/src/executables.js
@@ -1,7 +1,9 @@
 const { registerCommand } = require("./subprocess");
 
 const termuxNotification = registerCommand('termux-notification');
+const git = registerCommand('git');
 
 module.exports = {
     termuxNotification,
+    git,
 }
