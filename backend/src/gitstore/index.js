fa9302328928d9f5f48501618ed2fc4af1c16f2e
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-09 00:09:15 +0000

    refactor: streamline work tree management in transaction function

--- backend/src/gitstore/index.js
+++ backend/src/gitstore/index.js
@@ -21,7 +21,6 @@ class GitStoreClass {
     async getWorkTree() {
         if (!this.workTree) {
             this.workTree = await makeTemporaryWorkTree();
-            reset(this.gitDirectory, this.workTree);
         }
         return this.workTree;
     }
@@ -37,29 +36,30 @@ class GitStoreClass {
 }
 
 /**
+ * @typedef {GitStoreClass} GitStore
+ */
+
+/**
  * This function performs a transaction on a Git repository.
- * 
+ *
  * It gives you a temporary work tree, reset to the last commit,
  * and allows you to perform a transformation on it.
- * 
+ *
  * It is atomic: if the transformation fails, the changes are not committed.
- * 
+ *
  * @param {string} git_directory - The `.git` directory
- * @param {function(string): Promise<void>} transformation - A function that takes a directory path and performs some operations on it
+ * @param {function(GitStore): Promise<void>} transformation - A function that takes a directory path and performs some operations on it
  * @returns {Promise<void>}
  */
 async function transaction(git_directory, transformation) {
-    const temporaryWorkTree = await makeTemporaryWorkTree();
+    const store = new GitStoreClass(git_directory);
     try {
-        reset(git_directory, temporaryWorkTree);
-        await transformation(temporaryWorkTree);
-        await commit(
-            git_directory,
-            temporaryWorkTree,
-            "Diary audio files transcribed"
-        );
+        const workTree = await store.getWorkTree();
+        reset(git_directory, workTree);
+        await transformation(store);
     } finally {
-        await fs.rm(temporaryWorkTree, { recursive: true, force: true });
+        const workTree = await store.getWorkTree();
+        await fs.rm(workTree, { recursive: true, force: true });
     }
 }
 
