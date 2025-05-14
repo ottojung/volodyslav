dea89054a3a41dbbf79ca0ed5a8e9e2607a4225a
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-14 04:33:51 +0000

    style: standardize quotes in /ping endpoint code

--- backend/src/routes/ping.js
+++ backend/src/routes/ping.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const runtimeIdentifier = require('../runtime_identifier');
+const runtimeIdentifier = require("../runtime_identifier");
 
 /**
  * The alive check endpoint.
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
-router.get('/ping', (req, res) => {
-  const id = req.query.runtime_identifier;
-  if (id !== undefined) {
-    if (!id) {
-      return res.status(400).send('Bad Request');
+router.get("/ping", (req, res) => {
+    const id = req.query.runtime_identifier;
+
+    if (id !== undefined) {
+        if (!id) {
+            return res.status(400).send("Bad Request");
+        }
+        if (id !== runtimeIdentifier) {
+            return res.status(400).send("Identifiers do not match.");
+        }
     }
-    if (id !== runtimeIdentifier) {
-      return res.status(400).send('Bad Request');
-    }
-  }
-  return res.send('pong');
+
+    return res.send("pong");
 });
 
 module.exports = router;
