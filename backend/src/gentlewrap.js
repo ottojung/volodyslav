26581c86d335624dbb1f5ad3a58b488faebe168a
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-14 20:46:04 +0000

    refactor: improve formatting and readability in gentleCall function

--- backend/src/gentlewrap.js
+++ backend/src/gentlewrap.js
@@ -1,10 +1,10 @@
-/** 
+/**
  * The purpose of this module is provide better error messages for the
  * users of Volodyslav.
- * 
+ *
  * It is a wrapper around the main entry point of the application.
  * It catches any User errors that are thrown and logs them to the console.
- * 
+ *
  */
 
 const { logError } = require("./logger");
@@ -18,7 +18,10 @@ async function gentleCall(fn, errorsList) {
     try {
         return await fn();
     } catch (e) {
-        if (e instanceof Error && errorsList.some(predicate_1 => predicate_1(e))) {
+        if (
+            e instanceof Error &&
+            errorsList.some((predicate) => predicate(e))
+        ) {
             // If the error is a user error, log it to the console.
             logError({}, e.message);
             process.exit(1);
