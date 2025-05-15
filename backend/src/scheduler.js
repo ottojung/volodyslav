4e6f2be232034c2145682ed8bd2df67d855bbe55
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-15 23:18:01 +0000

    refactor: update everyHour function to pass capabilities object to processDiaryAudios for improved structure

--- backend/src/scheduler.js
+++ backend/src/scheduler.js
@@ -1,14 +1,17 @@
-
 // const cron = require('node-cron');
 const { processDiaryAudios } = require("./diary");
 const deleterCapability = require("./filesystem/deleter");
-const random = require('./random');
+const random = require("./random");
 
 async function everyHour() {
     const deleter = deleterCapability.make();
     const rng = random.default_generator(random.nondeterministic_seed());
+    const capabilities = {
+        deleter,
+        rng,
+    };
 
-    await processDiaryAudios(deleter, rng);
+    await processDiaryAudios(capabilities);
 }
 
 async function setup() {
