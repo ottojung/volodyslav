87b8d69eb86fa1239eb391e7ba68a5b1f5419518
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-15 02:30:48 +0000

    fix: include isCommandUnavailable in user error exports

--- backend/src/user_errors.js
+++ backend/src/user_errors.js
@@ -1,4 +1,5 @@
 const { isEnvironmentError } = require("./environment");
 const { isNotificationsUnavailable } = require("./notifications");
+const { isCommandUnavailable } = require("./subprocess");
 
-module.exports = [isEnvironmentError, isNotificationsUnavailable];
+module.exports = [isEnvironmentError, isNotificationsUnavailable, isCommandUnavailable];
