3f82aa50082b1745955d3259b85fb91ebfd1e188
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-16 01:03:26 +0000

    refactor: streamline capabilities usage in scheduler by importing from root

--- backend/src/capabilities/root.js
+++ backend/src/capabilities/root.js
@@ -1,4 +1,14 @@
 
+const deleterCapability = require("../filesystem/deleter");
+const random = require("../random");
+const dirscanner = require("../filesystem/dirscanner");
 
+const deleter = deleterCapability.make();
+const rng = random.default_generator(random.nondeterministic_seed());
+const scanner = dirscanner.make();
 
-
+module.exports = {
+    deleter,
+    rng,
+    scanner,
+};
--- backend/src/scheduler.js
+++ backend/src/scheduler.js
@@ -1,19 +1,8 @@
 // const cron = require('node-cron');
 const { processDiaryAudios } = require("./diary");
-const deleterCapability = require("./filesystem/deleter");
-const random = require("./random");
-const dirscanner = require("./filesystem/dirscanner");
+const capabilities = require("./capabilities/root");
 
 async function everyHour() {
-    const deleter = deleterCapability.make();
-    const rng = random.default_generator(random.nondeterministic_seed());
-    const scanner = dirscanner.make();
-    const capabilities = {
-        deleter,
-        rng,
-        scanner,
-    };
-
     await processDiaryAudios(capabilities);
 }
 
