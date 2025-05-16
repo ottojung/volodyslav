2b55c66b72ac40a112c9a470572bf3fe2e733fdd
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-16 02:48:22 +0000

    feat: add copier, creator, and writer capabilities to root module

--- backend/src/capabilities/root.js
+++ backend/src/capabilities/root.js
@@ -1,6 +1,9 @@
 const deleterCapability = require("../filesystem/deleter");
 const random = require("../random");
 const dirscanner = require("../filesystem/dirscanner");
+const copierCapability = require("../filesystem/copier");
+const creatorCapability = require("../filesystem/creator");
+const writerCapability = require("../filesystem/writer");
 const memconst = require("../memconst");
 
 const make = memconst(() => {
@@ -8,6 +11,9 @@ const make = memconst(() => {
         deleter: deleterCapability.make(),
         seed: random.seed.make(),
         scanner: dirscanner.make(),
+        copier: copierCapability.make(),
+        creator: creatorCapability.make(),
+        writer: writerCapability.make(),
     };
 });
 
