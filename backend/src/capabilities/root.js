1f9701ec1545cde13321bbe6f563f630dd2818a3
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-16 02:50:37 +0000

    refactor: reorder imports and enhance documentation for capabilities initialization

--- backend/src/capabilities/root.js
+++ backend/src/capabilities/root.js
@@ -1,10 +1,16 @@
-const deleterCapability = require("../filesystem/deleter");
+/**
+ * These capabilities are created at the very top of the call stack.
+ * This way, only the main entry to the program can grant these capabilities to the rest of the program.
+ */
+
+const memconst = require("../memconst");
+
 const random = require("../random");
+const deleterCapability = require("../filesystem/deleter");
 const dirscanner = require("../filesystem/dirscanner");
 const copierCapability = require("../filesystem/copier");
 const creatorCapability = require("../filesystem/creator");
 const writerCapability = require("../filesystem/writer");
-const memconst = require("../memconst");
 
 const make = memconst(() => {
     return {
