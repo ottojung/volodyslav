0e36e3322388fcdf503ef2e16495a14719e2ce40
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-05-22 00:02:11 +0000

    refactor: clean up unused type definitions and streamline Capabilities typedef

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,35 +4,8 @@ const commander = require("commander");
 const runtimeIdentifier = require("./runtime_identifier");
 const root = require("./capabilities/root");
 
-/** @typedef {import('./filesystem/deleter').FileDeleter} FileDeleter */
-/** @typedef {import('./random/seed').NonDeterministicSeed} NonDeterministicSeed */
-/** @typedef {import('./filesystem/dirscanner').DirScanner} DirScanner */
-/** @typedef {import('./filesystem/copier').FileCopier} FileCopier */
-/** @typedef {import('./filesystem/writer').FileWriter} FileWriter */
-/** @typedef {import('./filesystem/appender').FileAppender} FileAppender */
-/** @typedef {import('./filesystem/creator').FileCreator} FileCreator */
-/** @typedef {import('./filesystem/checker').FileChecker} FileChecker */
-/** @typedef {import('./subprocess/command').Command} Command */
-/** @typedef {import('./environment').Environment} Environment */
-/** @typedef {import('./logger').Logger} Logger */
-/** @typedef {import('./notifications').Notifier} Notifier */
-/** @typedef {import('./schedule').Scheduler} Scheduler */
-
 /**
- * @typedef {object} Capabilities
- * @property {NonDeterministicSeed} seed - A random number generator instance.
- * @property {FileDeleter} deleter - A file deleter instance.
- * @property {DirScanner} scanner - A directory scanner instance.
- * @property {FileCopier} copier - A file copier instance.
- * @property {FileWriter} writer - A file writer instance.
- * @property {FileAppender} appender - A file appender instance.
- * @property {FileCreator} creator - A directory creator instance.
- * @property {FileChecker} checker - A file checker instance.
- * @property {Command} git - A command instance for Git operations.
- * @property {Environment} environment - An environment instance.
- * @property {Logger} logger - A logger instance.
- * @property {Notifier} notifier - A notifier instance.
- * @property {Scheduler} scheduler - A scheduler instance.
+ * @typedef {import('./capabilities/root').Capabilities} Capabilities
  */
 
 /**
