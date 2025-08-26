38998313e3edf115b8ceac6121174ba7a2c55cf9
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-08-26 22:26:14 +0000

    Simplify cron expression comparison in task validation

--- backend/src/schedule/validation.js
+++ backend/src/schedule/validation.js
@@ -75,11 +75,11 @@ function validateTasksAgainstPersistedStateInner(registrations, persistedTasks) 
             extra.push(regTask.name);
         } else if (!taskIdentitiesEqual(regTask, persistedTask)) {
             // Detailed difference analysis
-            if (!cronScheduler.parseCronExpression(regTask.cronExpression).equal(cronScheduler.parseCronExpression(persistedTask.cronExpression))) {
+            if (regTask.cronExpression !== persistedTask.cronExpression) {
                 differing.push({
                     name: regTask.name,
                     field: 'cronExpression',
-                    expected: persistedTask.cronExpression, // FIXME: Better printed expression. This is currently the result of `unparse()` call, which is a mess.
+                    expected: persistedTask.cronExpression,
                     actual: regTask.cronExpression
                 });
             }
