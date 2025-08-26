bc63da56cbcd53f546fd0cc7a5d714135f6c1f58
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-08-26 07:10:02 +0000

    Refactor scheduling module exports: replace persistCurrentState with mutateTasks for improved clarity and consistency.

--- backend/src/cron/scheduling/index.js
+++ backend/src/cron/scheduling/index.js
@@ -5,7 +5,7 @@
 
 const { findPreviousFire, getMostRecentExecution } = require("./previous_fire_calculator");
 const { calculateMinimumCronInterval, validateTaskFrequency } = require("./frequency_validator");
-const { loadPersistedState, persistCurrentState } = require("./state_persistence");
+const { loadPersistedState, mutateTasks } = require("./state_persistence");
 const { makeTaskExecutor } = require("./task_executor");
 
 module.exports = {
@@ -14,6 +14,6 @@ module.exports = {
     calculateMinimumCronInterval,
     validateTaskFrequency,
     loadPersistedState,
-    persistCurrentState,
+    mutateTasks,
     makeTaskExecutor,
 };
