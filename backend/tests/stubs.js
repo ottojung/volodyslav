2a307beddcc14245e16612384bc0bf1db2c01c2f
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-08-28 23:23:39 +0000

    Remove direct overrides of POLL_INTERVAL_MS from polling module and its index in stubPollInterval function

--- backend/tests/stubs.js
+++ backend/tests/stubs.js
@@ -312,13 +312,9 @@ function stubRuntimeStateStorage(capabilities) {
 const stubPollInterval = (period = 1) => {
     // Direct module constant override for both old and new module locations
     const pollingSchedulerModule = require('../src/scheduler/polling/make');
-    const pollingModule = require('../src/scheduler/polling');
-    const pollingModuleIndex = require('../src/scheduler/polling/index');
     const intervalModule = require('../src/scheduler/polling/interval');
     pollingSchedulerModule.POLL_INTERVAL_MS = period;
-    pollingModule.POLL_INTERVAL_MS = period;
     intervalModule.POLL_INTERVAL_MS = period;
-    pollingModuleIndex.POLL_INTERVAL_MS = period;
 };
 
 module.exports = {
