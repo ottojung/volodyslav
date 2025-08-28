b5ee978aa4558b35ca601f7037073ad5e126ea83
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-08-28 23:23:14 +0000

    Remove unused POLL_INTERVAL_MS export from polling module

--- backend/src/scheduler/polling/index.js
+++ backend/src/scheduler/polling/index.js
@@ -3,10 +3,8 @@
  * Encapsulates all functionality related to polling lifecycle management.
  */
 
-const { POLL_INTERVAL_MS } = require('./interval');
 const { makePollingScheduler } = require('./make');
 
 module.exports = {
-    POLL_INTERVAL_MS,
     makePollingScheduler,
 };
