0e3ba5cd42828815cf0c9b417a191143ccde2124
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-09-02 22:03:16 +0000

    Fix import path for weekday utility functions in tests

--- backend/tests/weekday_string_functionality.test.js
+++ backend/tests/weekday_string_functionality.test.js
@@ -5,7 +5,7 @@
 const { DateTime: LuxonDateTime } = require("luxon");
 const { parseCronExpression, matchesCronExpression } = require("../src/scheduler");
 const DateTime = require("../src/datetime/structure");
-const { getAllWeekdayNames, isWeekdayName, luxonWeekdayToName, weekdayNameToCronNumber } = require("../src/datetime");
+const { getAllWeekdayNames, isWeekdayName, luxonWeekdayToName, weekdayNameToCronNumber } = require("../src/datetime/weekday");
 
 describe("Weekday string functionality", () => {
     test("DateTime.weekday should return weekday names", () => {
