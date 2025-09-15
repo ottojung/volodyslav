36ad73a637b308e494a81e8a744822be4d8f3d02
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-09-15 22:52:31 +0000

    Update KaTeX stylesheet configuration in docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -17,7 +17,12 @@ module.exports = {
         mermaid: true,
     },
     stylesheets: [
-        { href: 'katex/katex.min.css', type: 'text/css' },
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css',
+            type: 'text/css',
+            integrity: 'sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP',
+            crossorigin: 'anonymous',
+        },
     ],
     presets: [
         [
