325fd6f14a6260c7a9f41c2f3f04f0a0cb12646a
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-10-19 07:00:41 +0000

    fix: add node and git to package installation in Termux CI test script

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -7,5 +7,5 @@ export DEBIAN_FRONTEND=noninteractive
 cd /workspace
 apt-get update -q
 apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
-apt-get install -y coreutils file
+apt-get install -y coreutils file node git
 uname -a
