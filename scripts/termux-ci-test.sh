734f6e34e1ec6935ad9a19098b35658649dcec43
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-10-19 06:59:35 +0000

    fix: remove unnecessary Dpkg options in package installation

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -7,5 +7,5 @@ export DEBIAN_FRONTEND=noninteractive
 cd /workspace
 apt-get update -q
 apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
-apt-get install -y -o Dpkg::Options::="--force-confnew" coreutils file
+apt-get install -y coreutils file
 uname -a
