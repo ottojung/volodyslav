ac83697b511950ef9c1eaf081f23b216f952fbbc
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-10-19 07:01:08 +0000

    fix: update directory navigation in Termux CI test script

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -4,8 +4,11 @@ set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 
-cd /workspace
+cd -- "${0%/*}"/..
+
 apt-get update -q
 apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 apt-get install -y coreutils file node git
 uname -a
+
+
