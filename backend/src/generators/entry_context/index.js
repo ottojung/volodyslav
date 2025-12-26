af07ed749d468bded357bf78533835bb144bcf33
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-12-26 07:36:09 +0000

    Reformat code

--- backend/src/generators/entry_context/index.js
+++ backend/src/generators/entry_context/index.js
@@ -1,39 +1,38 @@
-
 /** @typedef {import('../../event').Event} Event */
 
-const { extractHashtags, isContextEnhancing } = require('../../event');
+const { extractHashtags, isContextEnhancing } = require("../../event");
 
 /**
  * This function extracts the context of a given entry from a list of all entries.
- * 
+ *
  * @param {Array<Event>} all_entries
  * @param {Event} entry
  * @returns {Array<Event>} The context of the given entry
  */
 function getEntryContext(all_entries, entry) {
     const entryHashtags = extractHashtags(entry);
-    
+
     // If entry has no hashtags, return empty context
     if (entryHashtags.size === 0) {
         return [];
     }
-    
+
     const context = all_entries.filter((otherEntry) => {
         // Skip the entry itself
         if (otherEntry.id === entry.id) {
             return false;
         }
-        
+
         // Check if entry is before the target entry in time
         if (!otherEntry.date.isBefore(entry.date)) {
             return false;
         }
-        
+
         // Check if entry type is context-enhancing
         if (!isContextEnhancing(otherEntry.type)) {
             return false;
         }
-        
+
         // Check if entry contains some of the same hashtags
         const otherHashtags = extractHashtags(otherEntry);
         for (const hashtag of otherHashtags) {
@@ -41,10 +40,10 @@ function getEntryContext(all_entries, entry) {
                 return true;
             }
         }
-        
+
         return false;
     });
-    
+
     return context;
 }
 
