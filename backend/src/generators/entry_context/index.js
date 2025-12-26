9f398f2ffb759b2a2a585ee0b43b4376f6a6fe59
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-12-26 06:59:38 +0000

    Add entry_context signature

---
+++ backend/src/generators/entry_context/index.js
@@ -0,0 +1,22 @@
+
+/** @typedef {import('../../event').Event} Event */
+
+/**
+ * 
+ * @param {Array<Event>} _all_entries
+ * @param {Event} _entry
+ * @returns {Array<Event>} The context of the given entry
+ */
+function entry_context(_all_entries, _entry) {
+    // TODO:
+    // Search for related entries.
+    // An entry is related to this `entry` if:
+    // - it is before it in time,
+    // - and it contains some of the same hashtags,
+    // - and it has type that has property `context_enhancing` set to true.
+    throw new Error("Not implemented yet");
+}
+
+module.exports = {
+    entry_context,
+};
