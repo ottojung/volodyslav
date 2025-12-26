7d23cb198799c5a48de3e29b8326818234670a92
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-12-26 07:11:28 +0000

    Fix typo in entry_context function comment for clarity on context enhancing property

--- backend/src/generators/entry_context/index.js
+++ backend/src/generators/entry_context/index.js
@@ -13,7 +13,7 @@ function entry_context(_all_entries, _entry) {
     // An entry is related to this `entry` if:
     // - it is before it in time,
     // - and it contains some of the same hashtags,
-    // - and it has type that has property `context_enhancing` set to true.
+    // - and it has type that has property `isContextEnhancing` set to true.
     throw new Error("Not implemented yet");
 }
 
