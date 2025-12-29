0073a6971ebefbb055d02d10e9e4f9b82f1be638
Otto Jung <121908118+ottojung@users.noreply.github.com>
2025-12-29 07:31:12 +0000

    Move out default graph creation

--- backend/src/generators/interface/class.js
+++ backend/src/generators/interface/class.js
@@ -6,84 +6,8 @@
 /** @typedef {import('../../event').Event} Event */
 /** @typedef {import('../dependency_graph').DependencyGraph} DependencyGraph */
 
-const { makeDependencyGraph, isUnchanged } = require("../dependency_graph");
-const { metaEvents, eventContext } = require("../individual");
-
-/**
- * Creates the default graph definition for the dependency graph.
- * @returns {Array<import('../dependency_graph/types').GraphNode>}
- */
-function createDefaultGraphDefinition() {
-    return [
-        {
-            output: "all_events",
-            inputs: [],
-            computor: (_inputs, oldValue) => {
-                // Pass-through for all_events - just return existing value
-                return oldValue || { type: "all_events", events: [] };
-            },
-        },
-        {
-            output: "meta_events",
-            inputs: ["all_events"],
-            computor: (inputs, oldValue) => {
-                const allEventsEntry = inputs[0];
-                if (!allEventsEntry) {
-                    return { type: "meta_events", meta_events: [] };
-                }
-
-                if (allEventsEntry.type !== "all_events") {
-                    return { type: "meta_events", meta_events: [] };
-                }
-
-                const allEvents = allEventsEntry.events;
-
-                /** @type {Array<import('../individual/meta_events').MetaEvent>} */
-                let currentMetaEvents = [];
-                if (oldValue && oldValue.type === "meta_events") {
-                    currentMetaEvents = oldValue.meta_events;
-                }
-
-                const result = metaEvents.computeMetaEvents(
-                    allEvents,
-                    currentMetaEvents
-                );
-
-                if (isUnchanged(result)) {
-                    return result;
-                }
-
-                return {
-                    type: "meta_events",
-                    meta_events: result,
-                };
-            },
-        },
-        {
-            output: "event_context",
-            inputs: ["meta_events"],
-            computor: (inputs) => {
-                const metaEventsEntry = inputs[0];
-                if (!metaEventsEntry) {
-                    return { type: "event_context", contexts: [] };
-                }
-
-                if (metaEventsEntry.type !== "meta_events") {
-                    return { type: "event_context", contexts: [] };
-                }
-
-                const metaEventsArray = metaEventsEntry.meta_events;
-                const contexts =
-                    eventContext.computeEventContexts(metaEventsArray);
-
-                return {
-                    type: "event_context",
-                    contexts: contexts,
-                };
-            },
-        },
-    ];
-}
+const { makeDependencyGraph } = require("../dependency_graph");
+const { createDefaultGraphDefinition } = require("./default_graph");
 
 /**
  * An interface for direct database operations.
