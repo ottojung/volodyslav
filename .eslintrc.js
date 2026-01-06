af3b8caf20e3e130ad3681f599344adb727130a7
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-01-06 01:38:03 +0000

    Add classIgnorePattern option to ESLint configuration for improved linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
                 vars: "all",
                 args: "after-used",
                 argsIgnorePattern: "^_",
+                classIgnorePattern: "^_",
             },
         ],
         "import/no-cycle": ["error", { maxDepth: "∞" }],
