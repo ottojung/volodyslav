ebb884b4bd72b2f970d60a82e04772a2772a3477
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-03-18 06:19:06 +0000

    add debug logging for asset directory path generation

--- backend/src/routes/entries/assets.js
+++ backend/src/routes/entries/assets.js
@@ -71,7 +71,9 @@ function entryAssetsDir(assetsDir, entry) {
     const year = date.year;
     const month = date.month.toString().padStart(2, "0");
     const day = date.day.toString().padStart(2, "0");
-    return path.join(assetsDir, `${year}-${month}`, day, entry.id.identifier);
+    const target = path.join(assetsDir, `${year}-${month}`, day, entry.id.identifier);
+    console.debug({target, entry});
+    return target;
 }
 
 /**
