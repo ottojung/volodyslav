4db29a116d83a6aff22a8f80d2f1ca8583d9dd97
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-03-18 06:26:12 +0000

    Revert "add debug logging for asset directory path generation"
    This reverts commit ebb884b4bd72b2f970d60a82e04772a2772a3477.

--- backend/src/routes/entries/assets.js
+++ backend/src/routes/entries/assets.js
@@ -71,9 +71,7 @@ function entryAssetsDir(assetsDir, entry) {
     const year = date.year;
     const month = date.month.toString().padStart(2, "0");
     const day = date.day.toString().padStart(2, "0");
-    const target = path.join(assetsDir, `${year}-${month}`, day, entry.id.identifier);
-    console.debug({target, entry});
-    return target;
+    return path.join(assetsDir, `${year}-${month}`, day, entry.id.identifier);
 }
 
 /**
