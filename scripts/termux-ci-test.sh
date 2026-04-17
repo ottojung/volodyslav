3e78614f7680a921b5c08c70def9a03a973c29b8
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-04-17 18:08:06 +0000

    fix: update npm ci command to ignore scripts in Termux CI script

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -12,7 +12,8 @@ apt-get install -q -y coreutils python build-essential file nodejs git
 uname -a
 id -a
 
+npm ci --ignore-scripts
 export PATH="$PWD/node_modules/.bin:$PWD/backend/node_modules/.bin:$PATH"
-npm ci
+npm rebuild --workspaces --include-workspace-root --foreground-scripts
 npm run build
 npm run test-only
