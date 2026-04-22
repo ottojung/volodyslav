d1079d5ff1d30061639fc1a0601d17d77a26866e
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-04-22 03:26:59 +0000

    only set LD_PRELOAD

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -13,24 +13,7 @@ uname -a
 id -a
 export
 
-export ANDROID_DATA='/data'
-export ANDROID_ROOT='/system'
-export DEBIAN_FRONTEND='noninteractive'
-export HISTCONTROL='ignoreboth'
-export HOME='/data/data/com.termux/files/home'
-export LANG='en_US.UTF-8'
 export LD_PRELOAD='/data/data/com.termux/files/usr/lib/libtermux-exec-ld-preload.so'
-export OLDPWD='/workspace'
-export PATH='/data/data/com.termux/files/usr/bin'
-export PREFIX='/data/data/com.termux/files/usr'
-export PWD='/workspace'
-export SHELL='/data/data/com.termux/files/usr/bin/bash'
-export SHLVL='1'
-export TERM='xterm'
-export TERMUX_MAIN_PACKAGE_FORMAT='debian'
-export TMPDIR='/data/data/com.termux/files/usr/tmp'
-export TZ='UTC'
-export _='/data/data/com.termux/files/usr/bin/sh'
 
 npm ci
 npm run build
