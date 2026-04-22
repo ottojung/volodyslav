b8a4f2510adaa011a8a0c03a7ebe15ef3354cfd1
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-04-22 03:36:02 +0000

    fix: correct LD_PRELOAD export syntax in termux-ci-test.sh

--- scripts/termux-ci-test.sh
+++ scripts/termux-ci-test.sh
@@ -12,7 +12,7 @@ apt-get install -q -y coreutils file nodejs git
 uname -a
 id -a
 
-export LD_PRELOAD='${PREFIX}/lib/libtermux-exec-ld-preload.so'
+export LD_PRELOAD="${PREFIX}/lib/libtermux-exec-ld-preload.so"
 
 npm ci
 npm run build
