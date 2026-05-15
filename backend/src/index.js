93c4fa1bf87202b5f122d5ab654583770eb1030d
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-05-14 19:13:54 -0700

    Revert backend/script exports and lint-ignore scripts for deep imports

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,63 @@
-const generators = require("./generators");
+const { gentleCall } = require("./gentlewrap");
+const { start } = require("./server");
+const commander = require("commander");
+const runtimeIdentifier = require("./runtime_identifier");
+const root = require("./capabilities");
 
-const capabilities = require("./capabilities");
+/**
+ * @typedef {import('./capabilities/root').Capabilities} Capabilities
+ */
+
+/**
+ * @param {Capabilities} capabilities
+ */
+async function printVersion(capabilities) {
+    const { version } = await runtimeIdentifier(capabilities);
+    capabilities.logger.printf(version);
+}
+
+/**
+ * @param {Capabilities} capabilities
+ */
+async function entryTyped(capabilities) {
+    const program = new commander.Command();
+
+    program.name("volodyslav").description("Volodyslav Media Service CLI");
+
+    program
+        .option("-v, --version", "Display the version")
+        .argument("[cmd]", "Command to execute")
+        .action(async (cmd, options) => {
+            if (options.version) {
+                await printVersion(capabilities);
+                capabilities.exiter.exit(0);
+            }
+            if (cmd) {
+                capabilities.logger.logError({ cmd }, `Unknown command ${JSON.stringify(cmd)}`);
+                program.help({ error: true });
+            } else {
+                program.outputHelp();
+            }
+        });
+
+    program
+        .command("start")
+        .description("Start the server")
+        .action(start(capabilities));
+
+    await program.parseAsync(process.argv);
+}
+
+async function entry() {
+    const capabilities = root.make();
+    return await gentleCall(capabilities, () => entryTyped(capabilities));
+}
+
+// Set up the command line interface with commander
+if (require.main === module) {
+    entry();
+}
 
 module.exports = {
-    makeInterface: generators.makeInterface,
-    DATABASE_SUBPATH: generators.DATABASE_SUBPATH,
-    makeRootCapabilities: capabilities.make,
+    entry,
 };
--- backend/tests/index.js
+++
@@ -1,7 +0,0 @@
-const migrationFixtureHelpers = require("./migration_fixture_helpers");
-const incrementalRemoteStub = require("./stub_incremental_database_remote");
-
-module.exports = {
-    forceVersion: migrationFixtureHelpers.forceVersion,
-    stubIncrementalDatabaseRemoteBranches: incrementalRemoteStub.stubIncrementalDatabaseRemoteBranches,
-};
