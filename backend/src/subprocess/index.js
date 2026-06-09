aaebd18336851d47efb7921ec771f2680b354e52
Otto Jung <otto.jung@vauplace.com>
2026-06-09 16:26:16 -0700

    fix imports

--- backend/src/subprocess/index.js
+++ backend/src/subprocess/index.js
@@ -1,8 +1,10 @@
 
 const { registerCommand } = require('./command');
+const { isProcessFailedError } = require('./call');
 const { isCommandUnavailable } = require('./resolve_executable_path');
 
 module.exports = {
     registerCommand,
     isCommandUnavailable,
+    isProcessFailedError,
 };
